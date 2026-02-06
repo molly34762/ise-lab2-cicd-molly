ca62adbdfd7d6209623abe7b4d4d2a633d901199
meixinliang <meixinliang8@gmail.com>
2026-02-06 21:46:00 +0000

    modified github actions

--- setup.sh
+++ setup.sh
@@ -46,28 +46,36 @@ WORKLOAD_IDENTITY_POOL_NAME=github-provider
 WORKLOAD_IDENTITY_POOL_ID="projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${WORKLOAD_IDENTITY_POOL_NAME}"
 WORKLOAD_IDENTITY_PROVIDER="projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${WORKLOAD_IDENTITY_POOL_NAME}/providers/${OIDC_NAME}"
 
-gcloud iam workload-identity-pools create "${WORKLOAD_IDENTITY_POOL_NAME}" \
-    --project="${PROJECT_ID}" \
-    --location="global" \
-    --display-name="GitHub Actions Pool"
+echo "Checking for Workload Identity Pool..."
+gcloud iam workload-identity-pools describe "${WORKLOAD_IDENTITY_POOL_NAME}" --project="${PROJECT_ID}" --location="global" >/dev/null 2>&1
 
-if [ $? != 0 ]; then
-    echo "creating a workload identity pool failed!"
-    exit 1
+if [ $? -ne 0 ]; then
+  echo "Workload Identity Pool not found. Creating..."
+  gcloud iam workload-identity-pools create "${WORKLOAD_IDENTITY_POOL_NAME}" \
+      --project="${PROJECT_ID}" \
+      --location="global" \
+      --display-name="GitHub Actions Pool"
+  if [ $? -ne 0 ]; then echo "creating a workload identity pool failed!"; exit 1; fi
+else
+  echo "Workload Identity Pool already exists."
 fi
 
-gcloud iam workload-identity-pools providers create-oidc "${OIDC_NAME}" \
-    --project="${PROJECT_ID}" \
-    --location="global" \
-    --workload-identity-pool="${WORKLOAD_IDENTITY_POOL_NAME}" \
-    --display-name="My GitHub repo Provider" \
-    --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner" \
-    --attribute-condition="assertion.repository_owner == '${GITHUB_ORG}'" \
-    --issuer-uri="https://token.actions.githubusercontent.com"
+echo "Checking for OIDC Provider..."
+gcloud iam workload-identity-pools providers describe "${OIDC_NAME}" --project="${PROJECT_ID}" --location="global" --workload-identity-pool="${WORKLOAD_IDENTITY_POOL_NAME}" >/dev/null 2>&1
 
-if [ $? != 0 ]; then
-    echo "creating an oidc failed!"
-    exit 1
+if [ $? -ne 0 ]; then
+  echo "OIDC Provider not found. Creating..."
+  gcloud iam workload-identity-pools providers create-oidc "${OIDC_NAME}" \
+      --project="${PROJECT_ID}" \
+      --location="global" \
+      --workload-identity-pool="${WORKLOAD_IDENTITY_POOL_NAME}" \
+      --display-name="My GitHub repo Provider" \
+      --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner" \
+      --attribute-condition="assertion.repository_owner == '${GITHUB_ORG}'" \
+      --issuer-uri="https://token.actions.githubusercontent.com"
+  if [ $? -ne 0 ]; then echo "creating an oidc failed!"; exit 1; fi
+else
+  echo "OIDC Provider already exists."
 fi
 
 gcloud iam service-accounts add-iam-policy-binding "${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
